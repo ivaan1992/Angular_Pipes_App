cf385e860f3dea6de8fb39e7908ff4527a93d949
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-14 15:39:50 -0500

    Add the uppercase, lowercase and titlecase pipes to my app

--- src/app/sales/pages/basics/basics.component.ts
+++ src/app/sales/pages/basics/basics.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
   styleUrls: []
 })
-export class BasicsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class BasicsComponent {
+  nameLower   : string = 'ivan';
+  nameUpper   : string = 'IVAN';
+  nameComplete: string = 'isMaEl IvaN LinArEs gaOna';
 
 }
