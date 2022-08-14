ea6e8a4d4fde3a70f61764d06433c1bbb35ba8ff
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-14 14:39:40 -0500

    Create sections in the menubar

--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -15,27 +15,34 @@ export class MenuComponent implements OnInit {
   ngOnInit(): void {
     this.items = [
       {
-          label: 'File',
-             items: [{
-                     label: 'New',
-                     icon: 'pi pi-fw pi-plus',
-                     items: [
-                         {label: 'Project'},
-                         {label: 'Other'},
-                     ]
-                 },
-                 {label: 'Open'},
-                 {label: 'Quit'}
-             ]
-         },
-         {
-             label: 'Edit',
-             icon: 'pi pi-fw pi-pencil',
-             items: [
-                 {label: 'Delete', icon: 'pi pi-fw pi-trash'},
-                 {label: 'Refresh', icon: 'pi pi-fw pi-refresh'}
-             ]
-         }
+        label: 'Angular Pipes',
+        icon: 'pi pi-desktop',
+        items: [
+          {
+            label: 'Texts & Dates',
+            icon: 'pi pi-align-left',
+            routerLink: '/'
+          },
+          {
+            label: 'Numbers',
+            icon: 'pi pi-dollar',
+            routerLink: 'numbers'
+          },
+          {
+            label: 'Not Commons',
+            icon: 'pi pi-globe',
+            routerLink: 'not-commons'
+          },
+          {
+            label: 'Order',
+            icon: 'pi pi-sort-alt',
+            routerLink: 'order'
+          }]
+      },
+      {
+        label: 'My Own Pipes',
+        icon: 'pi pi-star',
+      }
     ];
   }
 }
