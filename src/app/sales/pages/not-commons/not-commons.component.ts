266472ab2cf32531029522986cc42cd51097185a
Ivan Gaona <ivaan.lg.92@gmail.com>
2022-08-15 20:20:20 -0500

    Add slice pipe to my app

--- src/app/sales/pages/not-commons/not-commons.component.ts
+++ src/app/sales/pages/not-commons/not-commons.component.ts
@@ -24,7 +24,7 @@ export class NotCommonsComponent {
 
   // i18nPlural Pipe
 
-  clients: string[] = ['Ivan','Ivan','Ivan','Ivan','Ivan','Ivan','Ivan'];
+  clients: string[] = ['Ivan','Aaron','Edwin','Billy','Chris','Zack','John'];
   clientsMap = {
     '=0': 'We do not have any customer waiting.',
     '=1': 'We only have 1 customer waiting.',
